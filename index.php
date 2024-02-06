7a86be2bafc2bcd1d2a8fd03ac7b8c519d3a0abd
Italo David Ramos Ling <italo.ramos@ctic.pe>
2024-02-06 11:47:27 -0500

    change name js

--- index.php
+++ index.php
@@ -67,9 +67,9 @@
             </footer>
          </div>
 
-    <script src="JS/result.js"></script>
+    <script src="js/result.js"></script>
     <script src="https://code.jquery.com/jquery-3.5.1.js"></script>
-    <script src="JS/index.js"></script>
+    <script src="js/index.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/js/bootstrap.bundle.min.js"></script>
 </body>
 </html>
