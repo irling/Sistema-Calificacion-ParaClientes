f6c0cf13c6672993de1801d78b05753793e442ae
italo <italodavidramosling@gmail.com>
2024-02-08 16:47:12 -0500

    se añadio el icono de la web

--- index.php
+++ index.php
@@ -5,7 +5,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Califiquenos</title>
+    <title>Califiquennos</title>
     <link rel="stylesheet" href="http://localhost/sistema-ctic/css/index.css">
     <link rel="stylesheet" href="css/index.css">
     <link rel="stylesheet" href=/medias/mediaQueris-index.css>
