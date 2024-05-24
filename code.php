dd242f9b9466d949be7356cf591ead9dba7c3c27
Italo Ramos Ling <58456422+irling@users.noreply.github.com>
2024-05-23 21:45:09 -0500

    Update code.php
    actualizando

--- code.php
+++ code.php
@@ -2,13 +2,9 @@
 
 function ConectarBD(){
     //$servidor ="localhost";
-    $servidor ="bh8978.banahosting.com";
     //$usuario ="root";
-    $usuario ="jfafqddq_irling";
-    $clave="vFlnxflm5yxU";
     //$clave="";
     //$clave="root"; warpserver --
-    $bd="jfafqddq_c4l1fic4t10n";
     //$bd="prueba";
     $cnx=mysqli_connect($servidor,$usuario,$clave,$bd);
     return $cnx;
